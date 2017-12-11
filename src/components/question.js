c1ecc5c7c32bcbed7fdd8b2c43bd302e3e0ca46e
laramontana <larabrightside@gmail.com>
2017-12-12 00:43:05 +0300

    Add CSS styling

--- src/components/question.js
+++ src/components/question.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const Question = ({ question }) =>
-    <div >
-          <p>{question.q}</p>
-          <p>{question.a}</p>
-      </div>
+    <div>
+      <h2>{question.q}</h2>
+    </div>
 
 export default Question;
