60e6a6f4164143231e517587fe4964c6a4e0afd8
laramontana <larabrightside@gmail.com>
2017-12-12 02:00:20 +0300

    Add Next button and next Question rendering

--- src/App.js
+++ src/App.js
@@ -21,13 +21,22 @@ class App extends Component {
     })
   }
 
+  handleClickNext = event => {
+    this.setState({
+      answerClicked: false
+    })
+    this.componentDidMount()
+  }
+
   componentDidMount() {
-      this.props.fetchQuestion()
+    this.props.fetchQuestion()
   }
 
   render() {
     const {question} = this.props
     let answer = this.state.answerClicked ? <Answer question={question}/> : null;
+    let next = this.state.answerClicked ? <button onClick={this.handleClickNext} type="button" className="btn btn-outline-warning">Next Question</button> : null;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -37,6 +46,7 @@ class App extends Component {
         <div className="App-intro">
           <Question question={question}/>
           <button onClick={this.handleClick} type="button" className="btn btn-outline-warning">Answer</button>
+          {next}
           {answer}
         </div>
       </div>
