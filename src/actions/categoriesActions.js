3b50beb9240f7e90c607e41b6a3724a5c72aad21
laramontana <larabrightside@gmail.com>
2017-12-12 15:44:57 +0300

    Minor correction in fetching categories and categories list displaying

--- src/actions/categoriesActions.js
+++ src/actions/categoriesActions.js
@@ -3,7 +3,7 @@ export function fetchCategories() {
 
   return function(dispatch){
     dispatch({type: 'LOADING'})
-    return fetch('http://www.jservice.io/api/categories?count=50')
+    return fetch('http://www.jservice.io/api/categories?count=48')
       .then(res => {
         return res.json()
       }).then(responseJson => {
