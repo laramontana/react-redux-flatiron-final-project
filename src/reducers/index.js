edbb875123a65aea90da44a0d5fc402a0febfec4
laramontana <larabrightside@gmail.com>
2017-12-12 14:03:23 +0300

    Add categoriesReducer.js (wip)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import question from './questionsReducer';
+import categories from './categoriesReducer';
+
 
 export default combineReducers({
   question,
+  categories
 });
