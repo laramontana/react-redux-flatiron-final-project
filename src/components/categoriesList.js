198abb43971f0b5b42e27aaf39e9c198fa992c95
laramontana <larabrightside@gmail.com>
2017-12-12 15:35:05 +0300

    Add categories buttons styling

--- src/components/categoriesList.js
+++ src/components/categoriesList.js
@@ -3,8 +3,7 @@ import '../App.css';
 
 const CategoriesList = ({categories}) =>
     <div>
-      <h2>categoriesList</h2>
-      {categories.map(category=><button onClick={this.handleClickNext} type="button" className="cat">{category.title.charAt(0).toUpperCase() + category.title.slice(1)}</button>)}
+      {categories.map(category=><button key={category.id} onClick={this.handleClickNext} type="button" className="cat">{category.title.charAt(0).toUpperCase() + category.title.slice(1)}</button>)}
     </div>
 
 export default CategoriesList;
